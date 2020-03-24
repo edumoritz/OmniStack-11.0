2013472715d6aea876fc613ac4ee60175a4a31af
edumoritz <eduardo.moritz@hotmail.com>
2020-03-24 19:24:03 -0300

    backend da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação) - request.query
+ * Route Params: Parâmetros utilizados para identificar recursos - request.params
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos - request.body
+ */
+module.exports = {
+    async index (request, response) {
+        const ongs = await connection('ongs').select('*');
+
+        return response.json(ongs);
+    },
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');    
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+
+        return response.json({ id });
+    }
+}
